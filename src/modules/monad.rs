5887cf4fd660d526bc4a8e3fd70a9bfeac9942f0
Joshfairhead <logins@joshafairhead.com>
2025-05-27 23:10:36 +0100

    implement dyad

--- src/modules/monad.rs
+++ src/modules/monad.rs
@@ -7,27 +7,19 @@ pub struct Monad {
 impl Monad {
     pub const TERM_ATTRIBUTE_DESCRIPTION: &'static str = "Represents unity in diversity and diversity in unity.";
 
+    /// Creates a new monad
     pub fn new(name: &str) -> Self {
         Monad {
             name: name.to_string(),
             terms: Vec::new(),
         }
     }
-
-    /// Adds a term to the Monad's list of terms.
-    ///
-    /// # Arguments
-    ///
-    /// * `term` - The term string to add.
+    /// Adds a term to the monad vector
     pub fn add_term(&mut self, term: &str) {
         self.terms.push(term.to_string());
     }
 
-    /// Retrieves all terms associated with the Monad.
-    ///
-    /// # Returns
-    ///
-    /// A reference to the vector of term strings.
+    /// Retrieves all terms associated with the Monad vector.
     pub fn get_all_terms(&self) -> &Vec<String> {
         &self.terms
     }
