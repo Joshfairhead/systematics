9cbcd1aee5165bce70b49c981d3137e7204661b4
Joshfairhead <logins@joshafairhead.com>
2025-06-03 21:26:27 +0100

    add permutations module

--- target/debug/deps/systematics-00447e5745feb749.d
+++ target/debug/deps/systematics-00447e5745feb749.d
@@ -1,6 +1,6 @@
-/Users/joshfairhead/Dev/systematics/target/debug/deps/systematics-00447e5745feb749: src/main.rs src/modules/mod.rs src/modules/monad.rs src/modules/dyad.rs src/modules/triad.rs src/modules/tetrad.rs src/modules/pentad.rs src/modules/hexad.rs src/modules/heptad.rs src/modules/octad.rs src/modules/dodecad.rs
+/Users/joshfairhead/Dev/systematics/target/debug/deps/systematics-00447e5745feb749: src/main.rs src/modules/mod.rs src/modules/monad.rs src/modules/dyad.rs src/modules/triad.rs src/modules/tetrad.rs src/modules/pentad.rs src/modules/hexad.rs src/modules/heptad.rs src/modules/octad.rs src/modules/dodecad.rs src/modules/permutations.rs
 
-/Users/joshfairhead/Dev/systematics/target/debug/deps/systematics-00447e5745feb749.d: src/main.rs src/modules/mod.rs src/modules/monad.rs src/modules/dyad.rs src/modules/triad.rs src/modules/tetrad.rs src/modules/pentad.rs src/modules/hexad.rs src/modules/heptad.rs src/modules/octad.rs src/modules/dodecad.rs
+/Users/joshfairhead/Dev/systematics/target/debug/deps/systematics-00447e5745feb749.d: src/main.rs src/modules/mod.rs src/modules/monad.rs src/modules/dyad.rs src/modules/triad.rs src/modules/tetrad.rs src/modules/pentad.rs src/modules/hexad.rs src/modules/heptad.rs src/modules/octad.rs src/modules/dodecad.rs src/modules/permutations.rs
 
 src/main.rs:
 src/modules/mod.rs:
@@ -13,3 +13,4 @@ src/modules/hexad.rs:
 src/modules/heptad.rs:
 src/modules/octad.rs:
 src/modules/dodecad.rs:
+src/modules/permutations.rs:
